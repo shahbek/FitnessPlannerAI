22f25f2a2c85a6036fb26af256e3e2a59c1027f5
Shahbek Miru <60500115+shahbek@users.noreply.github.com>
2025-10-20 19:34:29 -0700

    feat: index for all components

---
+++ src/prompt-builders/index.ts
@@ -0,0 +1,13 @@
+// Prompt Builders Index
+// Exports all prompt builders for easy importing
+
+export { FeasibilityPromptBuilder } from './FeasibilityPromptBuilder';
+export { MetabolicPromptBuilder } from './MetabolicPromptBuilder';
+export { TrainingFrameworkPromptBuilder } from './TrainingFrameworkPromptBuilder';
+export { ExerciseLibraryPromptBuilder } from './ExerciseLibraryPromptBuilder';
+export { WeeklyOutlinesPromptBuilder } from './WeeklyOutlinesPromptBuilder';
+export { SessionTemplatesPromptBuilder } from './SessionTemplatesPromptBuilder';
+export { NutritionStrategyPromptBuilder } from './NutritionStrategyPromptBuilder';
+export { MealTemplatesPromptBuilder } from './MealTemplatesPromptBuilder';
+export { ShoppingListPromptBuilder } from './ShoppingListPromptBuilder';
+export { ValidationPromptBuilder } from './ValidationPromptBuilder';
